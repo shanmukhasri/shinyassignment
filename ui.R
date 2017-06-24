fc4622a63efdb8deea7616d9a1b5671b61549df6
kalidindi shanmugha sri siva teja <shanmughasri@gmail.com>
2017-06-24 08:43:55 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,21 @@
+#UI.R
+
+
+
+
+library(shiny)
+shinyUI(fluidPage(
+        titlePanel("Iris Dataset"),
+        sidebarLayout(
+                sidebarPanel(
+                        radioButtons("x", "Select X-axis:",
+                                     list("Sepal.Length"='a', "Sepal.Width"='b', "Petal.Length"='c', "Petal.Width"='d')),
+                        radioButtons("y", "Select Y-axis:",
+                                     list("Sepal.Length"='e', "Sepal.Width"='f', "Petal.Length"='g', "Petal.Width"='h'))
+                ),
+                mainPanel(
+                        strong(p("select the x axis variable and y axis variable to analyse the relation")),
+                        plotOutput("distPlot")
+                )
+        )
+))
